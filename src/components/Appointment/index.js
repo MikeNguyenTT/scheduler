f711435322a332a1f4cbf4a58ac4efb4ab395602
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-02-24 13:16:11 -0500

    making API request for Scheduler Day data

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import "components/Appointment/styles.scss";
+import Show from "./Show";
+import Header from "./Header";
+import Empty from "./Empty";
+
+export default function Appointment(props) {
+   
+  return (
+    <Fragment>
+      {props.time && <Header time={props.time} />}
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </Fragment>
+   );
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+import React from "react";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  
+  const dayItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+    });
+
+  function formatSpot() {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    return props.spots + " spots remaining"
+  }
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayItemClass}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpot(props.spots)} </h3>
+    </li>
+  );
+}
