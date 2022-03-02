809047e3f800bd022b096e53ec5ddd15c2c1259d
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-03-02 15:25:43 -0500

    purposely failing a test to break CircleCI build

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -4,8 +4,8 @@ export const SET_INTERVIEW = "SET_INTERVIEW";
 
 export default function reducer(state, action) {
   switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.value }
+    // case SET_DAY:
+    //   return { ...state, day: action.value }
     case SET_APPLICATION_DATA: {
       const { days, appointments, interviewers } = action;
       return {...state, days , appointments , interviewers}
