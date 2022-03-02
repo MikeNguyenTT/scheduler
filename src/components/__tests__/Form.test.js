a05254dd37551f610d755cce150de6919d97a2e4
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-03-02 17:21:53 -0500

    fix unit test failed after interviewer validation implementation

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Form from "components/Appointment/Form";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, getAllByAltText, getByTestId } from "@testing-library/react";
 import { fireEvent } from "@testing-library/react";
 
 afterEach(cleanup);
@@ -46,7 +46,7 @@ describe("Form", () => {
   
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
+    const { getByText, getByPlaceholderText, queryByText, getByTestId } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
   
@@ -59,12 +59,13 @@ describe("Form", () => {
       target: { value: "Lydia Miller-Jones" }
     });
   
+    fireEvent.click(getByTestId("interviewer-list-item"));
     fireEvent.click(getByText("Save"));
-  
+
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
   
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 
   it("calls onCancel and resets the input field", () => {
